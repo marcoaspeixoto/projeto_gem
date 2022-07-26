dcf4af401c9c689a7153bcadf92573c12be23858
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-25 22:35:59 -0300

    25-07-2022

--- lib/alo_marco.rb
+++ lib/alo_marco.rb
@@ -1,6 +1,11 @@
-require "alo_marco/version"
+require '../lib/alo_marco/version'
 
 module AloMarco
   class Error < StandardError; end
   # Your code goes here...
+  class Alo
+    def dizer
+      puts "Alo Marco"
+    end
+  end
 end
